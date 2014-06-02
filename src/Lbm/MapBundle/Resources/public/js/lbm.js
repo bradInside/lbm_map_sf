15bb2e09061747dd6cb130ef8ce71619cb009482
bradInside <brad.inside@gmail.com>
2014-06-02 15:30:45 +0200

    gestion listings

--- src/Lbm/MapBundle/Resources/public/js/lbm.js
+++ src/Lbm/MapBundle/Resources/public/js/lbm.js
@@ -3,6 +3,7 @@ var bounds;
 var geocoder;
 var marker;
 var infowindow;
+var autocomplete;
 
 var array_infowindow =[];
 var test;
@@ -35,8 +36,11 @@ function setMap() {
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     geocoder = new google.maps.Geocoder();
     bounds = new google.maps.LatLngBounds();
-
-
+    var input = /** @type {HTMLInputElement} */(document.getElementById('map_adress_input'));
+    autocomplete = new google.maps.places.Autocomplete(input);
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        codeAddress();
+    });
     return map;
 }
 
@@ -153,16 +157,16 @@ function addMarker(member) {
     });
     marker.lbmId = member.id;
     google.maps.event.addListener(marker, 'click', function () {
-        map.setCenter(marker.getPosition());
-        smoothZoom(map, 14, map.getZoom());
+        //map.setCenter(marker.getPosition());
+        //smoothZoom(map, 14, map.getZoom());
         marker.infowindow.open(map,marker);
     });
     infowindow = new google.maps.InfoWindow({
-        content: '<div id="content"><b>'+member.pseudo+'</b></div>'
+        content: '<div class="infowindow_content"><b>'+member.pseudo+'</b></div>'
     });
     array_infowindow.push(infowindow);
     marker.infowindow = infowindow;
-    infowindow.open(map,marker);
+    //infowindow.open(map,marker);
     return marker;
 }
 
@@ -179,3 +183,60 @@ function smoothZoom (map, max, cnt) {
         setTimeout(function(){map.setZoom(cnt)}, 80); // 80ms is what I found to work well on my system -- it might not work well on all systems
     }
 }
+
+
+function zoomOnMarker(markerId) {
+    var lg = lbm_members.length;
+    for (i=0;i<lg;i++) {
+       if(lbm_members[i].id == markerId){
+           var member = lbm_members[i];
+           var marker = member['gmap'];
+           map.setCenter(marker.getPosition());
+           marker.infowindow.open(map,marker);
+       }
+    }
+}
+
+function checkCodePostal(cp)
+{
+    // on test si c'est un code postal francais.
+    return cp.match(/^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/);
+}
+
+function codeAddress() {
+    /* Récupération de la valeur de l'adresse saisie */
+    var address = $('#map_adress_input').val();
+
+    if(checkCodePostal(address))
+    {
+        // si c'est un code postal, on modifie en rajoutant France derrière.
+    }
+
+    /* Appel au service de geocodage avec l'adresse en paramètre */
+    geocoder.geocode( { 'address': address,region:'fr' }, function(results, status) {
+        /* Si l'adresse a pu être géolocalisée */
+        if (status == google.maps.GeocoderStatus.OK) {
+            /* Récupération de sa latitude et de sa longitude */
+            var lat = results[0].geometry.location.lat();
+            var lng =  results[0].geometry.location.lng();
+
+            map.setCenter(results[0].geometry.location); // on centre la carte sur la recherche.
+            map.setZoom(14);
+            /* TEST GEOCODE */
+            /*
+             map.setCenter(results[0].geometry.location);
+             var marker = new google.maps.Marker({
+             map: map,
+             position: results[0].geometry.location
+             });
+                */
+            //DoSearch({'lat':lat,'lng':lng});
+        }else
+        {
+            alert('Cette ville n\'a pas été trouvée');
+        }
+    });
+}
+
+
+
